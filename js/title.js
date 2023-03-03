570bb79b45d4c6fb971e2bdac6c7ac0d8bcb13d0
Qlittlo <SuperQlittle@outlook.com>
2023-03-03 21:51:51 +0800

    Site updated: 2023-03-03 21:51:41

--- js/title.js
+++ js/title.js
@@ -8,7 +8,7 @@ document.addEventListener('visibilitychange', function () {
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = 'Qlittle | 🐖 I find you!';
+    document.title = 'Qlittle | 🐖 I found you!';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
